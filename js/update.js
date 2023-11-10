d63edcbf631dddfc65b6fb2d3bb48b9c1cdf64ce
[mdmohsin] <[roni07wd@gmail.com]>
2023-11-10 14:55:54 +0200

    update js file

--- js/update.js
+++ js/update.js
@@ -9,6 +9,12 @@
 
     const serverPath ='/persons';
 
+    const showResultSet = ()=> resultset.removeAttribute('class','hidden');
+    const hideResultSet = ()=> resultset.setAttribute('class','hidden');
+
+    const showMessageArea = ()=> messagearea.classList.remove('hidden');
+    const hideMessageArea = ()=> messagearea.classList.add('hidden');
+
     document.addEventListener('DOMContentLoaded', init);
 
     function init(){
@@ -20,7 +26,21 @@
 
         document.getElementById('submit').addEventListener('click', submit);
 
+        key.addEventListener('focus', clear);
+        key.addEventListener('change', ()=> search.focus());
+        search.addEventListener('change',submit);
+
+        clear();
+
     }// end of init
+
+    function clear(){
+        key.value = '';
+        search.vlaue = '';
+        hideMessageArea();
+        hideResultSet();
+        key.focus();
+    } // end of clear
 
     async function submit(){
         const searchKey = key.value;
@@ -30,8 +50,38 @@
         /* console.log('url',url); */
         const result = await fetch(url);
         const personData = await result.json();
+
+        updatePage(personData);
         /* console.log(personData); */
         /* messagearea.innerHTML = '<h2>Testing</h2>'; */
-    }
+    } //end of submit
+
+    function showMessagae(message){
+        messagearea.textContent=message;
+        hideResultSet();
+        showMessageArea();
+    } // end of showMessage
+
+    function updatePage(searchResult){
+        if (searchResult.message){
+showMessagae(searchResult.message);
+        }
+        else if (searchResult.length==0){
+            showMessagae('No person found');
+        }
+        else {
+            let htmlString='';
+            for (const person of searchResult){
+                htmlString+=`<tr>
+                <td>${person.firstname}</td>
+                <td>${person.lastname}</td>
+                <td>${person.age}</td>
+                </tr>\n`
+            }
+            resultarea.innerHTML=htmlString;
+            showResultSet();
+            hideMessageArea();
+        }
+    } //end of updatePage
 
 })(); //automatically running function.
