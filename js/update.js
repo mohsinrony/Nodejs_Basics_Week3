e077feb278f0a0437ea25296d61f5a0e53e920a1
[mdmohsin] <[roni07wd@gmail.com]>
2023-11-10 13:57:31 +0200

    fetch data using js

--- js/update.js
+++ js/update.js
@@ -22,8 +22,16 @@
 
     }// end of init
 
-    function submit(){
-        messagearea.innerHTML = '<h2>Testing</h2>';
+    async function submit(){
+        const searchKey = key.value;
+        const searchValue = search.value;
+
+        const url =searchKey?`${serverPath}/${searchKey}?value=${searchValue}` : serverPath;
+        /* console.log('url',url); */
+        const result = await fetch(url);
+        const personData = await result.json();
+        /* console.log(personData); */
+        /* messagearea.innerHTML = '<h2>Testing</h2>'; */
     }
 
 })(); //automatically running function.
